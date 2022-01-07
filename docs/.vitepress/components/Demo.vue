1b5ef10d22412bc22ce664e8574e96014e626db6
Jan Müller <janmueller3698@gmail.com>
2022-01-07 21:16:25 +0100

    docs: inline demo

--- docs/.vitepress/components/Demo.vue
+++ docs/.vitepress/components/Demo.vue
@@ -1,22 +1,34 @@
 <template>
-  <iframe
-    src="https://graph-controller-demo.yeger.eu"
-    title="Demo"
-    width="100%"
-  />
+  <GraphView :graph="graph" />
 </template>
+
+<script lang="ts" setup>
+import { defineNodeWithDefaults, GraphLink } from 'd3-graph-controller'
+
+const a = defineNodeWithDefaults({
+  id: 'a',
+  type: 'node',
+  color: 'var(--c-brand-light)',
+  label: {
+    color: 'black',
+    fontSize: '1rem',
+    text: 'A',
+  },
+})
+
+const graph = {
+  nodes: [a],
+  links: [] as GraphLink[],
+}
+</script>
 
 <style>
 .theme-default-content {
   max-width: unset !important;
 }
-</style>
 
-<style scoped>
-iframe {
-  border-radius: 0.5rem;
-  border-width: 0;
-  height: clamp(600px, 70vh, 100rem);
-  display: block;
+.graph {
+  border: 1px solid var(--c-border);
+  margin-top: 2rem;
 }
 </style>

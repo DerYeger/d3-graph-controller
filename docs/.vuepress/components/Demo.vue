b7a2441fda9b917410d0a740740f7eebb5d22bc1
Jan Müller <janmueller3698@gmail.com>
2022-01-08 10:59:15 +0100

    docs: create `Demo` component

---
+++ docs/.vuepress/components/Demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <iframe
+    src="https://graph-controller-demo.yeger.eu"
+    title="Demo"
+    width="100%"
+  />
+</template>
+
+<style>
+.theme-default-content {
+  max-width: unset !important;
+}
+</style>
+
+<style scoped>
+iframe {
+  border-width: 0;
+  height: clamp(600px, 70vh, 100rem);
+  display: block;
+}
+</style>
--- docs/demo/README.md
+++ docs/demo/README.md
@@ -5,16 +5,4 @@ contributors: false
 lastUpdated: false
 ---
 
-<iframe src="https://graph-controller-demo.yeger.eu" title="Demo" width="100%"></iframe>
-
-<style>
-.theme-default-content {
-  max-width: unset !important;
-}
-
-iframe {
-  border-width: 0;
-  height: clamp(600px, 70vh, 100rem);
-  display: block;
-}
-</style>
+<Demo />
